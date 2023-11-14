ea5422ffe0bbea6efa7524d75773fd55d69614df
sneach <khmilarseniy@mail.ru>
2023-11-14 21:24:20 +0700

    config change issues solved

--- app/src/main/java/com/example/fragmentnavttraining/MainActivity.kt
+++ app/src/main/java/com/example/fragmentnavttraining/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.fragmentnavttraining
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.fragmentnavttraining.contract.Router
 import com.example.fragmentnavttraining.ui.AFragment
+
 private const val MSG_KEY = "key"
 
 class MainActivity : AppCompatActivity(), Router {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().add(R.id.fragment_container, AFragment()).commit()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, AFragment())
+                .commit()
+        }
     }
 
-
     override fun launchFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().addToBackStack(null)
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .addToBackStack(null)
             .replace(R.id.fragment_container, fragment).commit()
     }
 
